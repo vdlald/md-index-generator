2e3755166d21e8f0acdc8c5d4acf8d5cf3910275
Vladislav Golubinov <vlad.vdlald@gmail.com>
2021-04-08 20:27:02 +0300

    read args

--- src/main/java/com/vladislav/mdindexgenerator/App.java
+++ src/main/java/com/vladislav/mdindexgenerator/App.java
@@ -1,9 +1,21 @@
 package com.vladislav.mdindexgenerator;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class App implements Runnable {
 
+  private final String pathToMdFile;
+
   public static void main(String[] args) {
-    new App().run();
+    if (args.length == 0) {
+      System.out.println("You need to specify the path to the md file.");
+      return;
+    }
+
+    final String pathToMdFile = args[0];
+
+    new App(pathToMdFile).run();
   }
 
   @Override
