8d4186e01f8e8ac428908a23fe147af495a3f8a7
Vladislav Golubinov <vlad.vdlald@gmail.com>
2021-04-08 21:36:58 +0300

    naive serialize

--- src/main/java/com/vladislav/mdindexgenerator/HeadElement.java
+++ src/main/java/com/vladislav/mdindexgenerator/HeadElement.java
@@ -1,13 +1,37 @@
 package com.vladislav.mdindexgenerator;
 
+import java.nio.charset.StandardCharsets;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
-import lombok.val;
 
 @ToString
 @RequiredArgsConstructor
 public class HeadElement {
 
+  private static final byte SYMBOL = '#';
+
+  @Getter
   private final String line;
 
+  public short getLevel() {
+    short level = 0;
+    final byte[] bytes = line.getBytes(StandardCharsets.UTF_8);
+
+    for (int i = 0; i < 6; i++) {
+      final byte aByte = bytes[i];
+      if (aByte == SYMBOL) {
+        level++;
+      } else {
+        break;
+      }
+    }
+
+    return level;
+  }
+
+  public String getLink() {
+    // TODO: 4/8/21 implement
+    return line;
+  }
 }
--- src/main/java/com/vladislav/mdindexgenerator/TableOfContentsSerializeStream.java
+++ src/main/java/com/vladislav/mdindexgenerator/TableOfContentsSerializeStream.java
@@ -8,13 +8,18 @@ public class TableOfContentsSerializeStream {
 
   private final Iterator<HeadElement> headers;
 
-  private final short currentDepth = 0;
-
   public boolean hasNext() {
     return headers.hasNext();
   }
 
   public String nextLine() {
-    return headers.next().toString();
+    final HeadElement headElement = headers.next();
+
+    final short level = headElement.getLevel();
+    final String line = headElement.getLine();
+    final String link = headElement.getLink();
+    final String tabs = "\t".repeat(level);
+
+    return String.format("%s[%s](%s)", tabs, line, link);
   }
 }
