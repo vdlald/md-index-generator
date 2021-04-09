2002d80bded959150a6239f77a1853a65808c860
Vladislav Golubinov <vlad.vdlald@gmail.com>
2021-04-09 09:59:38 +0300

    implement simpleTest

--- src/test/java/com/vladislav/mdindexgenerator/MainIntegrationTest.java
+++ src/test/java/com/vladislav/mdindexgenerator/MainIntegrationTest.java
@@ -1,22 +1,59 @@
 package com.vladislav.mdindexgenerator;
 
-import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import lombok.SneakyThrows;
 import org.junit.Test;
 
 public class MainIntegrationTest {
 
-  private static final String path = MainIntegrationTest.class.getResource("/TestInput.md")
-      .getPath();
+  @Test
+  public void simpleTest() {
+    final String[] args = {getInPath()};
+    StringBuffer out = new StringBuffer();
 
-  private String[] args;
+    Main main = new Main(args);
+    main.setOut(s -> out.append(s).append('\n'));
 
-  @Before
-  public void setUp() {
-    args = new String[]{path};
+    final String expected = getOut();
+
+    main.run();
+    final String result = out.toString();
+
+    assertEquals(expected, result);
   }
 
-  @Test
-  public void test() {
-    Main.main(args);
+  /**
+   * Looks at the stacktrace to see which test triggered the method.
+   * <p>
+   * You must call this method at the beginning of the test, and the output file must be stored in
+   * test/resource in a folder with the same name as the method
+   *
+   * @return expected out for specific test
+   */
+  @SneakyThrows
+  private String getOut() {
+    final String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
+    final String rawPath = MainIntegrationTest.class
+        .getResource(String.format("/%s/out.md", methodName)).getPath();
+    final Path path = Path.of(rawPath);
+    return new String(Files.readAllBytes(path));
+  }
+
+  /**
+   * Looks at the stacktrace to see which test triggered the method.
+   * <p>
+   * You must call this method at the beginning of the test, and the input file must be stored in
+   * test/resource in a folder with the same name as the method
+   *
+   * @return path to in file for specific test
+   */
+  @SneakyThrows
+  private String getInPath() {
+    final String path = String.format(
+        "/%s/in.md", Thread.currentThread().getStackTrace()[2].getMethodName());
+    return MainIntegrationTest.class.getResource(path).getPath();
   }
 }
---
+++ src/test/resources/simpleTest/out.md
@@ -0,0 +1,40 @@
+1. [My Project](#my-project)
+    1. [Idea](#idea)
+    2. [Implementation](#implementation)
+        1. [Step 1](#step-1)
+            1. [Some header](#some-header)
+        2. [Step 2](#step-2)
+            1. [Some header](#some-header-1)
+    3. [Article](#article)
+2. [Header 1](#header-1)
+    1. [Implementation](#implementation-1)
+
+# My Project
+
+## Idea
+
+content
+
+## Implementation
+
+### Step 1
+
+content
+
+#### Some header
+
+content
+
+### Step 2
+
+content
+
+#### Some header
+
+content
+
+## Article
+
+# Header 1
+
+## Implementation
